1ae941287cf29697ab964a242798e7104847c9cd
sgukz <passathorn.dev@gmail.com>
2020-12-21 17:22:25 +0700

    edit endpoint

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios");
-const moment = require("moment");
-const request = require("request");
-moment.locale("th");
 const app = express();
 app.use(
   bodyParser.urlencoded({
@@ -11,64 +8,11 @@ app.use(
   })
 );
 app.use(bodyParser.json());
-const APP_URL = "http://61.19.127.228:8089";
+const APP_URL = "http://61.19.127.228:5050";
 
-function formateDateTH(dateTime, style) {
-  let date = dateTime.split("-");
-  let day = parseInt(date[2]);
-  let month = parseInt(date[1]);
-  let strMonthCut = [
-    "",
-    "มกราคม",
-    "กุมภาพันธ์",
-    "มีนาคม",
-    "เมษายน",
-    "พฤษภาคม",
-    "มิถุนายน",
-    "กรกฎาคม",
-    "สิงหาคม",
-    "กันยายน",
-    "ตุลาคม",
-    "พฤศจิกายน",
-    "ธันวาคม",
-  ];
-  let year = parseInt(date[0]) + 543;
-
-  let createdDate =
-    style === 1
-      ? strMonthCut[month] + " " + year
-      : day + " " + strMonthCut[month] + " " + year;
-  //console.log(createdDate);
-  return createdDate;
-}
-// router test request
-app.post("/body", function (req, res) {
-  let userId = "";
-  if (req.body.events[0].source.groupId != undefined) {
-    userId = req.body.events[0].source.groupId;
-  } else {
-    userId = req.body.events[0].source.userId;
-  }
-  let formatMessage = {
-    type: "text",
-    text: JSON.stringify(req.body),
-  };
-  reply(userId, formatMessage);
-  res.sendStatus(200);
-});
-app.post("/webhook", function (req, res) {
-  let userId = "";
-  if (req.body.events[0].source.groupId != undefined) {
-    userId = req.body.events[0].source.groupId;
-  } else {
-    userId = req.body.events[0].source.userId;
-  }
-  let formatMessage = {
-    type: "text",
-    text: JSON.stringify(req.body),
-  };
-  reply(userId, formatMessage);
-  res.sendStatus(200);
+app.post("/getMeetingByDocno", function (req, res) {
+  const docno = req.body.docno
+  res.status(200).json(docno);
 });
 
 app.listen(process.env.PORT || 8000, function () {
